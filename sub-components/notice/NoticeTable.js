a962c8f9c91e5c650f129f31992502cbb54c3a35
Homin Lee <homi_yi@naver.com>
2023-10-05 17:43:02 +0900

    NoticeTable의 더미데이터를 NoticeData.js로 분리

--- sub-components/notice/NoticeTable.js
+++ sub-components/notice/NoticeTable.js
@@ -24,33 +24,7 @@ import {
 import {
     NoticeDetail
   } from 'sub-components'
-
-const noticeContent = {
-    no : '1',
-    title : '공지입니다',
-    writer : '작성자',
-    date : '작성일'
-}
-
-function List() {
-    const noticeContent = {
-        no : '1',
-        title : '공지입니다',
-        writer : '작성자',
-        date : '작성일'
-    }
-}
-
-function tableContent() {
-    return (
-        <tr>
-            <th scope="row">1</th>
-            <td>공지입니다</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-        </tr>
-    )
-}
+import NoticeData from 'data/dashboard/NoticeData';
 
 const NoticeTable = () => {
 	return (
@@ -59,9 +33,6 @@ const NoticeTable = () => {
                 <Card.Body>
                     <Table hover className="text-nowrap">
                         <thead className="table-light">
-                            {
-                                
-                            }
                             <tr>
                                 <th scope="col">#</th>
                                 <th scope="col">제목</th>
@@ -70,66 +41,13 @@ const NoticeTable = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>공지입니다</td>
-                                <td>Otto</td>
-                                <td>@mdo</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Jacob</td>
-                                <td>Thornton</td>
-                                <td>@fat</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">4</th>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">5</th>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">6</th>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">7</th>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">8</th>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">9</th>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">10</th>
-                                <td>Larry</td>
-                                <td>the Bird</td>
-                                <td>@twitter</td>
-                            </tr>
+                            {NoticeData.map((x, i) =>
+                            <tr key={i}>
+                                <th scope="row">{x.id}</th>
+                                <td>{x.title}</td>
+                                <td>{x.writer}</td>
+                                <td>{x.date}</td>
+                            </tr>)}
                         </tbody>
                     </Table>
                     <Button variant="secondary" className='createNotice'>글쓰기</Button>
