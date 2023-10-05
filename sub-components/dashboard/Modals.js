9d4ef0b1c558e96ea5b3228afec482c03c7264da
Homin Lee <homi_yi@naver.com>
2023-10-05 11:13:06 +0900

    Modals.js 파일 복구

--- sub-components/dashboard/Modals.js
+++ sub-components/dashboard/Modals.js
@@ -15,25 +15,24 @@ const Modals = (stockData) => {
                 인천-1동-1층
               </Modal.Title>
             </Modal.Header>
-            <Modal.Body>
-            
-                  {ProjectsData.map((item, index) => {
-                      if(item.locnatio == "인천-1동-1층"){
-                                        return (
-                                        <tr key={index}>
-                                        <td className="align-bottom">{item.number}</td>
-                                        <td className="align-bottom">{item.company}</td>
-                                        <td className="align-bottom">{item.projectName}</td>
-                                        <td className="align-bottom">{item.storing_quantity}</td>
-                                        <td className="align-bottom">{item.location}</td>
-                                        <td className="align-bottom">{item.classification}</td>
-                                        <td className="align-bottom">{item.contract_day}</td>
-                                        <td className="align-bottom">{item.phone}</td>
-                                        <td className="align-bottom">{item.address}</td>
-                                        </tr>
-                                            )}
-                                        })}
-                                
+            <Modal.Body>            
+              {ProjectsData.map((item, index) => {
+                  if(stockData.id == item.id){
+                                    return (
+                                    <tr key={index}>
+                                    <tr className="align-bottom">등록번호 : {item.number}</tr>
+                                    <tr className="align-bottom">회사명 : {item.company}</tr>
+                                    <tr className="align-bottom">입고제품명 : {item.projectName}</tr>
+                                    <tr className="align-bottom">상품분류 : {item.classification}</tr>
+                                    <tr className="align-bottom">입고수량 : {item.storing_quantity}</tr>
+                                    <tr className="align-bottom">입고위치 : {item.location}</tr>
+                                    <tr className="align-bottom">입고일 : {item.storing}</tr>
+                                    <tr className="align-bottom">계약일 : {item.contract_day}</tr>
+                                    <tr className="align-bottom">전화번호 : {item.phone}</tr>
+                                    <tr className="align-bottom">고객주소 : {item.address}</tr>
+                                    </tr>
+                                        )}
+                                    })}                                
             </Modal.Body>
             <Modal.Footer>
               <Button onClick={props.onHide}>닫기</Button>
